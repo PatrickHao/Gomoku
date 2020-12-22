d0e3c52065fb9fd31fadea66d1ad64f44b547835
PatrickHao <943526723@qq.com>
2020-12-22 19:50:21 +0800

    20201222

--- Gomoku/ClientControl.cs
+++ Gomoku/ClientControl.cs
@@ -4,20 +4,15 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-namespace Gomoku
-{
-    class ClientControl
-    {
+namespace Gomoku {
+    class ClientControl {
         private Socket clientSocket;
 
-
-        public ClientControl()
-        {
+        public ClientControl() {
             clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
-        public void Connect(string ip, int port)
-        {
+        public void Connect(string ip, int port) {
             clientSocket.Connect(ip, port);
             Console.WriteLine("连接服务器成功");
 
@@ -26,28 +21,19 @@ namespace Gomoku
             threadReceive.Start();
 
         }
-        public void Receive()
-        {
-            try
-            {
+        public void Receive() {
+            try {
                 byte[] msg = new byte[1024];
                 int msgLen = clientSocket.Receive(msg);
-                //Console.WriteLine("服务器说: " + Encoding.UTF8.GetString(msg, 0, msgLen));
-                //string pointStr = Encoding.UTF8.GetString(msg, 0,msgLen);
-
-                //MainForm.f.recData.Text += Encoding.UTF8.GetString(msg, 0, msgLen);
-
+                //process
 
                 Receive();
-            }
-            catch
-            {
+            } catch {
                 Console.WriteLine("服务器积极拒绝");
             }
         }
 
-        public void Send(string msg)
-        {
+        public void Send(string msg) {
             clientSocket.Send(Encoding.UTF8.GetBytes(msg));
         }
 
